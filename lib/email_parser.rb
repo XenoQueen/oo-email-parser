928720654c36bfbde7c66b9db1bfaca658e0fd80
Maika Kailani <queenmab051@gmail.com>
2019-12-10 16:23:54 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,14 +1,4 @@
 class EmailParser
-  attr_accessor :name, :csv_emails
+  attr_accessor :emails
   
-  def initialize(csv_emails)
-    @csv_emails = csv_emails
-  end
   
-  def parse
-    csv_emails.split.collect do |address|
-      address.split(',')
-    end
-    .flatten.uniq
-  end
-end

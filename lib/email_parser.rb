86f8642c2d2ee9a20f3b0ac3744a16b97d4ac452
Maika Kailani <queenmab051@gmail.com>
2019-12-10 16:27:29 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,14 +1,15 @@
 class EmailParser
-  attr_accessor :emails
-  
-  def initialize(emails)
-    @emails = emails
-  end
-  
-  def parse
-    puts splitEmail = emails.split.collect {|address| address.split(',')}
-    returnArr = splitEmail.flatten.uniq
-    returnArr
-    end
-  end
+attr_accessor :emails 
+
+def initialize(emails)
+  @emails = emails
+end 
+
+def parse
+  puts splitEmail = emails.split.collect {|address| address.split(',')} 
+  returnArr = splitEmail.flatten.uniq
+  returnArr
+end
+
+end
   
